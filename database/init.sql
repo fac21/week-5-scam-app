fbe1e0f7f8b6411e45ecd3ba60e227312f043bbd
Amy Kuner <amykuner1@gmail.com>
2021-04-21 17:00:34 +0100

    adding initial schema
    closes #11

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,26 @@
 BEGIN;
 
--- DROP TABLE IF EXISTS
+DROP TABLE IF EXISTS users, recipes, ingredients, match_recipe_ingredients CASCADE;
 
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE recipes (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(100) NOT NULL, 
+  user_id INTEGER REFERENCES users(id),
+);
+
+CREATE TABLE ingredients (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE match_recipe_ingredients (
+  recipe_id INTEGER REFERENCES recipes(id),
+  ingredient_id INTEGER REFERENCES ingredients(id)
+);
 
 COMMIT;
