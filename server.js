df182b8627e008a7f9e888053a42a39845e87742
Mariya Peychinova <mariapeichinova@gmail.com>
2021-04-22 10:35:41 +0100

    initialising homepage routs

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
 const server = express();
+
+const staticHandler = express.static("public");
+server.use(staticHandler);
+
+const homepageHandler = require("./routes/homepage.js");
+
+server.get("/", homepageHandler);
+
+
 
 
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => console.log (`Listening on http://localhost:${PORT}`));
