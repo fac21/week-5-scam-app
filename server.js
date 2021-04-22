be4f89005aa5d9d4212e619ac8e1edb13cf75613
Amy Kuner <amykuner1@gmail.com>
2021-04-22 16:28:37 +0100

    initialise addbd function

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const homepageFile = require("./src/handlers/homepage.js");
 const staticHandler = express.static("public");
 const addRecipe = require("./src/handlers/addreceipe.js")
 const PORT = process.env.PORT || 3000;
+const bodyParser = express.urlencoded();
 
 
 server.use(staticHandler);
@@ -11,7 +12,9 @@ server.use(staticHandler);
 server.get("/", homepageFile.homepageHandler);
 server.get("/add-recipe", addRecipe.addRecipe);
 
-server.post("/recipe-added", addRecipe.recipeAdded);
+server.post("/recipe-added", bodyParser, addRecipe.recipeAdded)
+
 server.post("/recipe-search", homepageFile.postForm); 
+
 
 server.listen(PORT, () => console.log (`Listening on http://localhost:${PORT}`));
--- src/handlers/addreceipe.js
+++ src/handlers/addreceipe.js
@@ -6,12 +6,50 @@ function addRecipe (request, response) {
     response.send(newHtml);
     response.redirect("/add-recipe")
     }
+
+function addToDb(request, response){
+  const info = request.body;
+  db.query("INSERT INTO users(name), recipes(name) VALUES($1) ($2)", [username] [recipe]);
+  //ingredients is its own funky issue
+}
       
 
 function recipeAdded(request, response) {
+  addToDb(request, response);
   response.send("<h1>Recipe added! Thank you for contributing to our database.</h1>")
   response.redirect("/recipe-added")
   
 }
 
 module.exports = {addRecipe: addRecipe, recipeAdded: recipeAdded};
+
+// INSERT INTO users (name) VALUES
+//   ('Chisha'),
+//   ('Saki'),
+//   ('Amy'),
+//   ('Mariya')
+// ;
+
+// INSERT INTO recipes (name, user_id) VALUES
+//   ('Carrot Cake', 1),
+//   ('Carrot and Ginger Soup', 2),
+//   ('Steak and Chips', 3),
+//   ('Carrot and Veggie Lasagne', 4)
+// ;
+
+// INSERT INTO ingredients (name) VALUES
+//   ('Carrots'),
+//   ('Flour'),
+//   ('Egg'),
+//   ('Ginger'),
+//   ('Steak'),
+//   ('Potatoes'),
+//   ('Peppers'),
+//   ('Tomatoes'),
+//   ('Pasta')
+// ;
+
+// INSERT INTO match_recipe_ingredients (recipe_id, ingredient_id) VALUES
+//   (1, 1),
+//   (1, 2)
+// ;
