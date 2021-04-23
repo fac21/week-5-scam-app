66dc3489da343a446ad202ebcd1790f998b0c230
Mariya Peychinova <mariapeichinova@gmail.com>
2021-04-23 11:26:33 +0100

    tests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,39 +1,58 @@
-// beforeEach(() => {
-//     cy.task("resetDb");
-//   });
+beforeEach(() => {
+    cy.task("resetDb");
+  });
 
-it("can run a test", () => {
-  assert.equal(1, 1);
+describe("run test", () => {
+  it("can run a test", () => {
+    assert.equal(1, 1);
+  });
 });
 
-it("can load the page", () => {
-  cy.visit("http://localhost:3000");
+describe("load page", () => {
+  it("can load the page", () => {
+    cy.visit("http://localhost:3000");
+  });
+});
+
+describe("load page", () => {
+  it("can load the page", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("input[type = 'button']").click();
+    cy.url().should('include', '/add-recipe');
+  });
 });
 
 // describe("list recipe", () => {
 //   it("displays a list of recipe", () => {
 //     cy.visit("http://localhost:3000");
-//     cy.contains("Sery1976");
-//     cy.contains("Spont1935");
+//     cy.contains("Carrot Cake");
+//     cy.contains("Steak and Chips");
 //   });
 // });
 
-it("can navigate to add-recipe page", () => {
-  cy.visit("http://localhost:3000/add-recipe");
+describe("navigate to add-recipe page", () => {
+  it("can navigate to add-recipe page", () => {
+    cy.visit("http://localhost:3000/add-recipe");
+  });
 });
 
-it("can input information", () => {
-  cy.visit("http://localhost:3000/add-recipe");
-  cy.get("#username").type("information");
-  cy.get("#recipe_name").type("recipe name")
-  cy.get('#ingredients').type('ingredient')
+describe("input information", () => {
+  it("can input information", () => {
+    cy.visit("http://localhost:3000/add-recipe");
+    cy.get("#username").type("information");
+    cy.get("#recipe_name").type("recipe name");
+    cy.get('#ingredients').type('ingredient');
+  });
 });
 
-// it("can submit information", () => {
-//   cy.visit("http://localhost:3000/add-recipe");
-//   cy.get("#message").type("information");
-//   cy.get("#name").type("my name")
-//   cy.get('form').submit()
-// });
-
+describe("add recipe", () => {
+  it("can add recipe", () => {
+    cy.visit("http://localhost:3000/add-recipe");
+    cy.get("#username").type("my name");
+    cy.get("#recipe_name").type("recipe name");
+    cy.get('#ingredients').type('ingredient')
+    cy.get('form').submit()
+  });
+});
+//test
 
