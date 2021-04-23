393b984c12f28e3f2657571b181a57f2932af12a
Amy Kuner <amykuner1@gmail.com>
2021-04-23 11:40:42 +0100

    adding database stuff

--- database/model.js
+++ database/model.js
@@ -1,14 +1,15 @@
 const db = require("./connection.js");
 
-// function getUsers() {
-//  return db.query("SELECT * FROM users")
-//.then((result) => result.rows);
+function getRecipes(request) {
+ return db.query("select recipes.name from recipes where id in (select match_recipe_ingredients.recipe_id from match_recipe_ingredients where id IN (select ingredients.id from ingredients where name = 'Carrots'));
+ , [search-filter])
+.then((result) => result.rows)
 //Catching any errors
-// .catch((error) => {
-//     console.error(error);
-//     // do stuff with the error here
-//   });
-// }
+.catch((error) => {
+    console.error(error);
+    // do stuff with the error here
+  });
+}
 
 
 // module.exports = { getUsers };
